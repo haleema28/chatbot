340f0aed49463652c40653d7236128b5b59b749f
Haleema sadiya <sadiyahaleema2004@gmail.com git config --global user.namehaleema28>
2024-12-13 18:47:57 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Generative AI project',
+    version='0.0.0.0',
+    author='haleema sadiya',
+    author_email='sadiyahaleema2004@gmail.com',
+    packages=find_packages(),
+    install_requires =[]
+)
